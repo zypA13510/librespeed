4f9e1a9342912890970794a64ca547cf4f4d2c0d
Bernd Stellwag <burned@zerties.org>
2020-10-10 18:41:06 +0200

    use correct name for accessing the X-Real-IP header via $_SERVER variable (#365)

--- backend/getIP.php
+++ backend/getIP.php
@@ -18,8 +18,8 @@ function getClientIp()
 {
     if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
         $ip = $_SERVER['HTTP_CLIENT_IP'];
-    } elseif (!empty($_SERVER['X-Real-IP'])) {
-        $ip = $_SERVER['X-Real-IP'];
+    } elseif (!empty($_SERVER['HTTP_X_REAL_IP'])) {
+        $ip = $_SERVER['HTTP_X_REAL_IP'];
     } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
         $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
         $ip = preg_replace('/,.*/', '', $ip); # hosts are comma-separated, client is first
